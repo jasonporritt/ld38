d87858d01f779e2d2067c21c4d928aee1fdb49f2
Jason Porritt <jason.porritt@atomicobject.com>
2017-04-23 16:49:59 -0400

    Updates!

--- sdl_sim_block.cpp
+++ sdl_sim_block.cpp
@@ -8,9 +8,9 @@
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-const int CELL_ROWS = 8;
-const int CELL_COLS = 10;
-const int CELL_SPACING = 1;
+const int CELL_ROWS = 48;
+const int CELL_COLS = 60;
+const int CELL_SPACING = 0;
 const int ROAD_WIDTH = 20;
 const int MENU_WIDTH = 138;
 
@@ -90,7 +90,7 @@ int cells[CELL_ROWS][CELL_COLS] = {
   {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
   {1, 0, 1, 1, 2, 1, 0, 0, 0, 0},
   {1, 0, 1, 2, 2, 1, 0, 0, 0, 0},
-  {1, 0, 1, 2, 3, 2, 1, 0, 0, 0},
+  {1, 0, 1, 2, 2, 2, 1, 0, 0, 0},
   {1, 0, 1, 1, 2, 1, 0, 0, 0, 0},
   {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
   {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
@@ -99,6 +99,7 @@ int cells[CELL_ROWS][CELL_COLS] = {
 int lastCells[CELL_ROWS][CELL_COLS] = {{0}};
 
 void initGameboard() {
+  srand(time(NULL));
   int max_cell_height = (SCREEN_HEIGHT - ROAD_WIDTH*2 - CELL_SPACING*(CELL_ROWS + 1)) / CELL_ROWS;
   int max_cell_width = (SCREEN_WIDTH - MENU_WIDTH - ROAD_WIDTH*2 - CELL_SPACING*(CELL_COLS + 1)) / CELL_COLS;
   cellHeight = cellWidth = std::min(max_cell_height, max_cell_width);
@@ -134,24 +135,35 @@ void clearScreen() {
   SDL_RenderClear( renderer );
 }
 
-int colors[3] = {0x66, 0xaa, 0xff};
+int residentialColors[5][3] = {
+  {0x0A, 0x5C, 0x34},
+  {0x1F, 0x83, 0x35},
+  {0x33, 0xAA, 0x35},
+  {0x7F, 0xC5, 0x77},
+  {0xCB, 0xE0, 0xB8}
+};
 void drawCells() {
   for (int r=0; r<CELL_ROWS; r++) {
     for (int c=0; c<CELL_COLS; c++) {
       SDL_Rect cellRect = { cellZeroX + c*(cellWidth + CELL_SPACING), cellZeroY + r*(cellHeight + CELL_SPACING), cellWidth, cellHeight};
-      SDL_SetRenderDrawColor( renderer, colors[cells[r][c]], colors[cells[r][c]], colors[cells[r][c]], 0xFF );
+      SDL_SetRenderDrawColor( renderer, residentialColors[cells[r][c]][0], residentialColors[cells[r][c]][1], residentialColors[cells[r][c]][2], 0xFF );
       SDL_RenderFillRect( renderer, &cellRect );
     }
   }
 }
 
 
-enum CellValue { LOW_VALUE_CELL, MEDIUM_VALUE_CELL, HIGH_VALUE_CELL, CellValue_MAX };
-enum CellType { STREET_CELL_TYPE, UNDEVELOPED_CELL_TYPE, RESEDENTIAL_CELL_TYPE, COMMERCIAL_CELL_TYPE, INDUSTRIAL_CELL_TYPE, CellType_MAX };
+enum CellValue {
+  ZERO_VALUE_CELL,
+  LOW_VALUE_CELL,
+  MEDIUM_VALUE_CELL,
+  HIGH_VALUE_CELL,
+  EXTREME_VALUE_CELL,
+  CellValue_MAX
+};
+enum CellType { UNDEVELOPED_CELL_TYPE, RESIDENTIAL_CELL_TYPE, COMMERCIAL_CELL_TYPE, INDUSTRIAL_CELL_TYPE, CellType_MAX };
 
-int contributingValues[9];
 int cellValueCounts[CellValue_MAX];
-int currentValue;
 int getNewCellValue(int (&cellMatrix)[CELL_ROWS][CELL_COLS], int row, int column) {
   std::fill(cellValueCounts, cellValueCounts+CellValue_MAX, 0);
   for (int r = row - 1; r <= row + 1 && r < CELL_ROWS; r++) {
@@ -161,25 +173,28 @@ int getNewCellValue(int (&cellMatrix)[CELL_ROWS][CELL_COLS], int row, int column
       if (c < 0)
         c = 0;
 
-      contributingValues[(r-row+1) + (r-row+1)*(c-column+1)] = cellMatrix[r][c];
       cellValueCounts[cellMatrix[r][c]] += 1;
     }
   }
 
-  currentValue = cellMatrix[row][column];
-  /*
-  int mostCommonValue = std::distance(cellValueCounts, std::max_element(cellValueCounts, cellValueCounts + CellValue_MAX));
-  int countOfMostCommonValue = *std::max_element(cellValueCounts, cellValueCounts + CellValue_MAX);
-  if (countOfMostCommonValue > 3) {
-    if (currentValue < mostCommonValue)
-      return currentValue + 1;
-    else if (currentValue > mostCommonValue)
-      return currentValue - 1;
+  int roll = rand() % 100 + 1;
+  int currentValue = cellMatrix[row][column];
+  int newTargetValue = currentValue;
+
+  int matchIfOver = 91;
+  for (int i=0; i<CellValue_MAX; i++) {
+    if (roll > matchIfOver && roll <= matchIfOver + cellValueCounts[i]) {
+      newTargetValue = i;
+    }
+    matchIfOver += cellValueCounts[i];
   }
-  return currentValue;
-  */
-  double average = std::accumulate(contributingValues, contributingValues + CellValue_MAX, 0) / ((double) CellValue_MAX);
-  return std::round(average);
+
+  if (newTargetValue > currentValue)
+    return currentValue + 1;
+  else if (newTargetValue < currentValue - 1)
+    return currentValue - 1;
+  else
+    return currentValue;
 }
 
 void updateCells() {
@@ -193,9 +208,27 @@ void updateCells() {
 
 int ticks = 0;
 void maybeUpdateCells() {
-  if (ticks % 60 == 0)
+  if (ticks % 10 == 0)
     updateCells();
   ticks += 1;
+}
+
+void handleEvents( SDL_Event* e )
+{
+  if(e->type == SDL_MOUSEBUTTONUP )
+  {
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+
+    // See if it was in a cell
+    bool isInCellsBoxX = x >= cellZeroX && x <= cellZeroX + (cellWidth + CELL_SPACING)*CELL_COLS;
+    bool isInCellsBoxY = y >= cellZeroY && y <= cellZeroY + (cellHeight + CELL_SPACING)*CELL_ROWS;
+    if (isInCellsBoxX && isInCellsBoxY) {
+      int col = (x - cellZeroX) / (cellWidth + CELL_SPACING);
+      int row = (y - cellZeroY) / (cellHeight + CELL_SPACING);
+      cells[row][col] = HIGH_VALUE_CELL;
+    }
+  }
 }
 
 int main( int argc, char* args[] )
@@ -220,7 +253,9 @@ int main( int argc, char* args[] )
       {
         quit = true;
       }
+      handleEvents(&e);
     }
+
     maybeUpdateCells();
 
     clearScreen();
